2514fe74de271f548577ec039593825298a24dbe
sleivaal <samu_5@hotmail.es>
2024-04-10 08:57:10 +0200

    Boton de Tweet añadido a la barra de navegacion en FeedController

--- TwitterClone/Controller/UploadTwitController.swift
+++ TwitterClone/Controller/UploadTwitController.swift
@@ -11,21 +11,53 @@ class UploadTwitController: UIViewController {
     
     //MARK: - Properties
     
+    /**
+     Boton de tweet de la barra de navegacion
+     */
+    private lazy var tweetButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .twitterBlue //color de fondo
+        button.setTitle("Tweet", for: .normal) //asi se pone el texto
+        button.titleLabel?.textAlignment = .center //asi se centra el texto programaticamente
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16) //cambio del tamaño del texto
+        button.setTitleColor(.white, for: .normal) //cambio de color del texto
+        button.setDimensions(width: 64, height: 32)
+        button.layer.cornerRadius = 32 / 2
+        button.addTarget(self, action: #selector(handleTweet), for: .touchUpInside)
+        return button
+    }()
+    
+    
     
     //MARK: - Lifecyrcle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+      configureUI()
     }
     
     //MARK: - Selectors
 
+    @objc func handleCancel(){
+        
+        self.dismiss(animated: true)
+
+    }
+    
+    @objc func handleTweet(){
+        print("DEBUG: tweet button action...")
+    }
+    
     //MARK: - Functions
     
+    /**
+     Metodo que se encarga de configurar la vista
+     */
     func configureUI(){
         
-        
+        view.backgroundColor = .white
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: tweetButton)
         
     }
     
